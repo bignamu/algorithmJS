4c9c45cc030a2beaccac9ad6c5f94a3f389fba25
hodlMaster <sunohare7@gmail.com>
2021-08-17 22:28:48 +0900

    main

--- Programmers/Greedy/Greedy_1.js
+++ Programmers/Greedy/Greedy_1.js
@@ -1,7 +1,37 @@
 function solution(n, lost, reserve) {
     let answer = 0;
+    lost.sort()
+    reserve.sort()
+    let re = new Set(reserve)
+    let lo = new Set(lost)
+    // console.log('reserve',...re)
+    // console.log('lost',...lo)
+    let diff = new Set([...re].filter(x => !lo.has(x)));
+    let diff2 = new Set([...lo].filter(x => !re.has(x)));
     
-    console.log('test')
+    // console.log(n-diff.size)
+    let cnt = 0
+    for (let uni of diff2){
+        let front = uni - 1
+        let tail = uni + 1
+        
+        if (diff.has(front)){
+            diff.delete(front)
+            cnt += 1    
+        }
+        else if (diff.has(tail)){
+            diff.delete(tail)
+            cnt += 1
+        }
+    }
     
+    
+    answer = cnt + n - diff2.size
+    console.log(answer)
     return answer;
 }
+let n, lost, reserve
+n = 5
+lost = [2,4]
+reserve = [3,1]
+solution(n,lost,reserve)
--- Programmers/Greedy/Greedy_2.js
+++ Programmers/Greedy/Greedy_2.js
@@ -1,7 +1,31 @@
-function solution(n, lost, reserve) {
-    let answer = 0;
+function solution(name) {
+    var answer = 0;
+    let len = name.length;
+    let namelist = name.split('')
     
-    console.log('test')
+    let cur = 0;
+
+    console.log(len,namelist,namelist.slice(-1)[0],cur)
+    
+    
+    for(let i = 1; i<len;i++){
+        let last = namelist.slice(-i)[0];
+        console.log(last)
+        namelist.unshift(last)
+    }
+    console.log(namelist);
+    
+    function change(start){
+        
+        if (curname != 'A'){
+            
+        }
+        
+    }
     
     return answer;
 }
+
+
+let name2 = 'JAN'
+solution(name2)
