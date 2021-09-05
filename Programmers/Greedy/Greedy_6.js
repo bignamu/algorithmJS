2d4319cd6869f9a48c0144e3391edbbec3b6f54f
hodlMaster <sunohare7@gmail.com>
2021-09-05 22:24:22 +0900

    main

---
+++ Programmers/Greedy/Greedy_6.js
@@ -0,0 +1,12 @@
+https://programmers.co.kr/learn/courses/30/lessons/42884?language=javascript
+
+
+function solution(routes) {
+    var answer = 0;
+    return answer;
+}
+
+let routes = [[-20,15], [-14,-5], [-18,-13], [-5,-3]]
+
+solution(routes)
+
